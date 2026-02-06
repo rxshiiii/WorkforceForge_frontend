efcae9014f4a57f0f5886c9a10f80d28befb7b5b
Rushikesh Ankaikar <rgankaikar@gmail.com>
2026-02-06 22:25:23 +0530

    Fix env variable access for CRA

--- src/api/mentorApi.js
+++ src/api/mentorApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 // change if your FastAPI runs elsewhere
 
 // ============================
